0b4395d5ddc135c19a155959279346cd7de67543
richzjc <zhangjianchuan@wallstreetcn.com>
2019-12-06 14:17:06 +0800

    add files

--- comiler/src/main/java/com/richzjc/comiler/ParameterProcessor.java
+++ comiler/src/main/java/com/richzjc/comiler/ParameterProcessor.java
@@ -5,12 +5,42 @@ import com.google.auto.service.AutoService;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
 
 @AutoService(Processor.class)
 public class ParameterProcessor extends AbstractProcessor {
+
+    private Elements elementUtils;
+    private Messager messager;
+    private Filer filer;
+    private Types typeUtils;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        elementUtils = processingEnv.getElementUtils();
+        messager = processingEnv.getMessager();
+        filer = processingEnv.getFiler();
+        typeUtils = processingEnv.getTypeUtils();
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        return super.getSupportedAnnotationTypes();
+    }
+
+    @Override
+    public SourceVersion getSupportedSourceVersion() {
+        return SourceVersion.latestSupported();
+    }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
